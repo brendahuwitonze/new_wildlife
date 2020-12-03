0f2e76ea901f262089ebb56cb17b41fb4eaee323
brendahuwitonze <uwitonzebrendah@gmail.com>
2020-12-03 11:51:58 +0200

    last style

--- src/test/java/AnimalsTest.java
+++ src/test/java/AnimalsTest.java
@@ -25,25 +25,7 @@ private Animals testAnimal;
         Animals testAnimal=setNewAnimal ();
         assertEquals("zebra", testAnimal.getName());
     }
-//    @Test
-//    public void getId_animalInstantiatesWithId(){
-//          Animals testAnimal=setNewAnimal ();
-//            testAnimal.save ();
-//        assertTrue(testAnimal.getId() > 0);
-//        }
 
-//    @Test
-//    public void getType_animalInstantiatesWithType_Type() {
-//        Animals testAnimal=setNewAnimal ();
-//        assertEquals("Non-endangered", testAnimal.getType());
-//    }
-//    @Test
-//    public void findById() {
-//        Animals testAnimal=setNewAnimal();
-//        testAnimal.save();
-//        Animals foundAnimal= Animals.find(testAnimal.getId());
-//        assertTrue(foundAnimal.equals(testAnimal));
-//    }
 @Test
 public void save_savesAnimalIdIntoDB() {
     Animals testAnimal=setNewAnimal ();
@@ -51,14 +33,6 @@ public void save_savesAnimalIdIntoDB() {
     Animals savedAnimals = Animals.find(testAnimal.getId());
     assertEquals(savedAnimals.getId(), testAnimal.getId());
 }
-//    @Test
-//    public void all_returnsAllInstancesOfAnimal_false() {
-//        Animals testAnimal=setNewAnimal ();
-//        testAnimal.save();
-//        Animals otherAnimal = new Animals("Deer");;
-//        otherAnimal.save();
-//        assertEquals(true, Animals.all().get(0).equals(testAnimal));
-//        assertEquals(true, Animals.all().get(1).equals(otherAnimal));
-//    }
+
 
 }
